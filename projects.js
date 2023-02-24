eeba19e4e4f7d4fc5b6c79dcd07d0ad92af69dc2
Zaiii1407 <mendozaalzailo@gmail.com>
2023-02-24 16:18:03 +0800

    my-website-commit

---
+++ projects.js
@@ -0,0 +1,199 @@
+<div class="products-4" id="portfolio">
+
+  <div id="products4-block" class="text-center">
+      <div class="container">
+          <div class="heading text-center mx-auto mb-5">
+              <h3 class="head">PROJECTS</h3>
+              <p>Do these things makes me happy.</p>
+            </div>
+          <input id="tab1" type="radio" name="tabs" checked>
+          <label class="tabtle" for="tab1">All Projects</label>
+
+          <input id="tab4" type="radio" name="tabs">
+          <label class="tabtle" for="tab4">Website</label>
+
+          <input id="tab2" type="radio" name="tabs">
+          <label class="tabtle" for="tab2">Videos</label>
+
+          <input id="tab3" type="radio" name="tabs">
+          <label class="tabtle" for="tab3">Photography</label>
+
+          <section id="content1" class="tab-content text-left">
+              <div class="d-grid grid-col-3">
+                  <div class="product">
+                      <a href="assets/images/g1.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g1.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g2.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g2.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g3.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g3.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g4.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g4.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g5.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g5.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                     
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g6.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g6.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  
+                  
+              </div>
+          </section>
+
+          <section id="content2" class="tab-content text-left">
+              <div class="d-grid grid-col-3">
+                  <div class="product">
+                      <a href="assets/images/g7.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g7.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g8.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g8.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                     
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g1.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g1.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                     
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g2.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g2.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                     
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g3.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g3.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g4.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g4.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+              </div>
+          </section>
+
+          <section id="content3" class="tab-content text-left">
+              <div class="d-grid grid-col-3">
+                  <div class="product">
+                      <a href="assets/images/g5.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g5.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                    
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g6.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g6.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                     
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g7.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g7.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+              </div>
+          </section>
+
+          <section id="content4" class="tab-content text-left">
+              <div class="d-grid grid-col-3">
+                  <div class="product">
+                      <a href="assets/images/g8.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g8.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+                  <div class="product">
+                      <a href="assets/images/g1.jpg" data-lightbox="example-set"
+                          data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
+                          <figure>
+                              <img src="assets/images/g1.jpg" class="img-responsive" alt="" />
+                          </figure>
+                      </a>
+                      
+                  </div>
+              </div>
+          </section>
+      </div>
+  </div>
+  <!-- Products4 block -->
+</div>
+ 
